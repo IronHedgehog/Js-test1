ab29b3dda46e1288b175521e73116fada26a9517
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-02-24 17:19:59 +0200

    a

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(jonas);
 // 1. New {} is created
 //•2. function is called, this = {}
 // •3. • {}• linked to prototype
-//•4. • function automaticafly return {}
+//•4. • function automatically return {}
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
